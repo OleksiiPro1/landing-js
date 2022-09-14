685879b113725797ff21bb7f60eba8a94ac6b886
Alexey Piltenko <ukr.top@ukr.net>
2022-09-15 00:06:46 +0200

    scrollTo

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-$('#link-to-footer').click(function() {
-  $.scrollTo($('#footer'), 1000);
+$('#to-contacts').click(function() {
+  $.scrollTo($('#contacts'), 1000);
 });
+
+$('#to-schedule-a-tour').click(function() {
+  $.scrollTo($('#schedule-a-tour'), 1000);
+});
+
+$('#to-schedule-a-tour2').click(function() {
+  $.scrollTo($('#schedule-a-tour'), 1000);
+});
+
+$('#to-rooms').click(function() {
+  $.scrollTo($('#rooms'), 1000);
+});
+
+$('#to-about-us').click(function() {
+  $.scrollTo($('#about-us'), 1000);
+});
+
